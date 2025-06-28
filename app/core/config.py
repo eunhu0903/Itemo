8a2304e198e55cf428c94739e440ff1451c55666
eunhoo0903 <eunhoo0903@naver.com>
2025-06-29 02:17:12 +0900

    chore: 환경변수 로드 및 필수 설정값 추가 (.env)

--- app/core/config.py
+++ app/core/config.py
@@ -4,3 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+if not SECRET_KEY:
+    raise ValueError("FastAPI 애플리케이션에 SECRET_KEY가 설정되지 않았습니다.")
