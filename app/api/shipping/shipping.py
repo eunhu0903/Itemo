35d4734cd0a91368cc177a1b5e94af7a9932cf49
eunhoo0903 <eunhoo0903@naver.com>
2025-06-30 22:44:14 +0900

    feat(api): 배송지 조회 API 엔드포인트 추가 (GET /shipping-addresses)

--- app/api/shipping/shipping.py
+++ app/api/shipping/shipping.py
@@ -1,17 +1,23 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from schemas.shipping import ShippingAddressesCreate
+from schemas.shipping import ShippingAddressesCreate, ShippingAddressesResponse
 from db.session import get_db
 from core.token import get_token_from_header
-from service.service_shipping import create_shipping_address
+from typing import List
+from service.service_shipping import create_shipping_address, read_shipping_address
 
 router = APIRouter(tags=["Shipping-addresses"])
 
 @router.post("/shipping-addresses")
-def get_shipping_addr(
+def post_shipping_addresses(
     address_data: ShippingAddressesCreate,
     token: str = Depends(get_token_from_header),
     db: Session = Depends(get_db)
 ):
     new_address = create_shipping_address(token, address_data, db)
     return {"message": "배송지가 등록되었습니다.", "shipping_address_id": new_address.id}
+
+@router.get("/shipping-addresses", response_model=List[ShippingAddressesResponse])
+def get_shipping_addresses(token: str = Depends(get_token_from_header), db: Session = Depends(get_db)):
+    address = read_shipping_address(token, db)
+    return address
