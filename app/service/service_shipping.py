b4c27a77b5da742333e112642b18f4b8be45ef12
eunhoo0903 <eunhoo0903@naver.com>
2025-06-30 20:28:30 +0900

    feat(service): 배송지 등록 기능 구현

---
+++ app/service/service_shipping.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session 
+from schemas.shipping import ShippingAddressesCreate
+from app.models.shipping import ShippingAddress
+from core.token import verify_token
+
+def create_shipping_address(token: str, address_data: ShippingAddressesCreate, db: Session) -> ShippingAddress:
+    user = verify_token(token, db)
+
+    new_address = ShippingAddress(
+        user_id=user.id,
+        recipient=address_data.recipient,
+        phone_number=address_data.phone_number,
+        address=address_data.address,
+        detail_address=address_data.detail_address,
+        postal_code=address_data.postal_code
+    )
+
+    db.add(new_address)
+    db.commit()
+    db.refresh(new_address)
+    return new_address
