d266d4f02f28e6fd5a84e28ad46c90276cee2dbd
eunhoo0903 <eunhoo0903@naver.com>
2025-07-01 22:59:01 +0900

    feat(schemas): ProfileResponse 스키마에 profile_image 필드 추가

--- app/schemas/profile.py
+++ app/schemas/profile.py
@@ -5,6 +5,7 @@ class ProfileResponse(BaseModel):
     id: int
     email: EmailStr
     username: Optional[str]
+    profile_image: Optional[str]
 
     class Config:
         from_attributes = True
