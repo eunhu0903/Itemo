44505c10b17a35d571d6d7fb8ad1ced2c4df2266
eunhoo0903 <eunhoo0903@naver.com>
2025-07-02 23:58:12 +0900

    feat(models): User 모델에 products 관계 설정

--- app/models/auth.py
+++ app/models/auth.py
@@ -18,3 +18,4 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     shipping_addresses = relationship("ShippingAddress", back_populates="user")
+    products = relationship("Product", back_populates="seller")
