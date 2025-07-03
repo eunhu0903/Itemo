a650fa0f98f42178dc5c0741ea014a8c64cc6b3f
eunhoo0903 <eunhoo0903@naver.com>
2025-07-03 13:55:19 +0900

    feat(api): 상품 전체 목록 조회 API 추가 (GET /products)

--- app/api/product/product.py
+++ app/api/product/product.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends, UploadFile, File, Form
 from sqlalchemy.orm import Session
 from core.token import verify_token, get_token_from_header
 from db.session import get_db
+from typing import List
 from schemas.product import ProductResponse
-from service.service_product import create_product
+from service.service_product import create_product, read_all_product
 
 router = APIRouter(tags=["Product"])
 
@@ -18,4 +19,9 @@ def post_product(
 ):
     user = verify_token(token, db)
     product = create_product(name, description, price, image, user, db)
+    return product
+
+@router.get("/products", response_model=List[ProductResponse])
+def get_all_product(db: Session = Depends(get_db)):
+    product = read_all_product(db)
     return product
