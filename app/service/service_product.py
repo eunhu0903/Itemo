4e0efadfe714b894faf207e7110ba893735b22f8
eunhoo0903 <eunhoo0903@naver.com>
2025-07-03 14:50:48 +0900

    feat(service): 상품 수정 기능 구현

--- app/service/service_product.py
+++ app/service/service_product.py
@@ -3,6 +3,7 @@ from fastapi import UploadFile, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from models.product import Product
+from schemas.product import ProductUpdate
 from models.auth import User
 from core.s3 import upload_fileobj
 
@@ -40,3 +41,30 @@ def read_product(products_id: int, db: Session) -> Product:
     if not product:
         raise HTTPException(status_code=404, detail="해당 상품을 찾을 수 없습니다.")
     return product
+
+def update_product(products_id: int, name: str, description: str, price: float, image: UploadFile | None, seller_id: User, db: Session):
+    product = db.query(Product).filter(Product.id == products_id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="해당 상품을 찾을 수 없습니다.")
+    if product.seller_id != seller_id.id:
+        raise HTTPException(status_code=403, detail="상품 수정 권한이 없습니다.")
+    
+    if image:
+        if not image.content_type.startswith("image/"):
+            raise HTTPException(status_code=400, detail="이미지 파일만 업로드 가능합니다.")
+        extension = image.filename.split(".")[-1]
+        object_name = f"product/{uuid.uuid4()}.{extension}"
+
+        image_url = upload_fileobj(image.file, object_name)
+        if not image_url:
+            raise HTTPException(status_code=500, detail="이미지 업로드 실패")
+        product.product_image = image_url
+    
+    product.name = name
+    product.description = description
+    product.price = price
+    
+    db.commit()
+    db.refresh(product)
+    return product
+    
